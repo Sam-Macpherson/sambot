abf3a24723305ca7677b2bed80f8830e49cbc2a6
Sam Macpherson <sam.macpherson15@gmail.com>
2020-10-31 11:52:04 -0400

    Added 'make' command to migrate.py to create an empty migration file.

--- migrate.py
+++ migrate.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import importlib
 from migrations import Migration
@@ -16,5 +17,31 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Wrong number of arguments.')
     else:
-        migration = sys.argv[1].zfill(4)
-        do_migration(migration)
+        if sys.argv[1].lower() == 'make':
+            number_of_migrations = len([
+                name for name in os.listdir('migrations')
+                if os.path.isfile(os.path.join('migrations', name))
+                and name.startswith('m_')
+            ])
+            new_migration = str(number_of_migrations + 1).zfill(4)
+            lines = [
+                f'from playhouse.migrate import *\n',
+                f'\n',
+                f'\n',
+                f'"""\n'
+                f'Migration {new_migration}.\n',
+                f'Add a description here.\n',
+                f'"""\n'
+                f'\n',
+                f'\n',
+                f'def migration():\n',
+                f'\tdatabase = SqliteDatabase(\'sambot.db\')\n',
+                f'\tmigrator = SqliteMigrator(database)\n',
+                f'\t# Write migration here.\n',
+                f'\t\n',
+            ]
+            with open(f'migrations/m_{new_migration}.py', 'w') as f:
+                f.writelines(lines)
+        else:
+            migration = sys.argv[1].zfill(4)
+            do_migration(migration)
